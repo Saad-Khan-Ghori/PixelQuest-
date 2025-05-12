d0ccfefa32bdd43da51910903cf23fc81f87caff
Saad Khan Ghori <109807094+Saad-Khan-Ghori@users.noreply.github.com>
2025-05-12 23:36:53 +0500

    Update Level.h

--- Game/Level.h
+++ Game/Level.h
@@ -9,11 +9,21 @@ public:
     static constexpr int TILE = 32;
     static constexpr int ROWS = 12;
     static constexpr int COLS = 100;   // 3200?px wide
-
+    Texture2D texPlatform;
+    Texture2D texGround;
+    Texture2D texLava;
+    Texture2D texGate;
     vector<vector<Tile>> map;
 
     Level();
+    void reset();
     void draw() const;
+    ~Level() {
+        UnloadTexture(texPlatform);
+        UnloadTexture(texGround);
+        UnloadTexture(texLava);
+        UnloadTexture(texGate);
+    }
 };
 
 #endif
